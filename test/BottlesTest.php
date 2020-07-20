aebfdb3b7552d986d0eba33473c1371541d84dfb
Katrina Owen <kytrinyx@github.com>
2020-07-20 14:33:07 -0600

    Add verse 2 test
    This fails.

--- test/BottlesTest.php
+++ test/BottlesTest.php
@@ -20,4 +20,13 @@ class BottlesTest extends \PHPUnit\Framework\TestCase {
       "2 bottles of beer on the wall.\n";
     $this->assertEquals($expected, (new Bottles())->verse(3));
   }
+
+  public function test_verse_2() {
+    $expected =
+      "2 bottles of beer on the wall, " .
+      "2 bottles of beer.\n" .
+      "Take one down and pass it around, " .
+      "1 bottle of beer on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verse(2));
+  }
 }
