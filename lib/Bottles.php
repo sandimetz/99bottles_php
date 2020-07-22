d6cc910b4be69d156b7d29364a549bc06513023b
Katrina Owen <kytrinyx@github.com>
2020-07-22 10:21:01 -0600

    Interpolate number in 2 case

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -30,10 +30,10 @@ class Bottles {
         "no more bottles of beer on the wall.\n";
     case 2:
       return
-        "2 bottles of beer on the wall, " .
-        "2 bottles of beer.\n" .
+        "{$number} bottles of beer on the wall, " .
+        "{$number} bottles of beer.\n" .
         "Take one down and pass it around, " .
-        "1 bottle of beer on the wall.\n";
+        ($number-1) . " bottle of beer on the wall.\n";
     default:
       return
         "{$number} bottles of beer on the wall, " .
