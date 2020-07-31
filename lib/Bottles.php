9c6de9c3a8ff0bfd506abc2a7c2c3b5a1e4d3be8
Katrina Owen <kytrinyx@github.com>
2020-07-31 14:29:48 -0600

    Flesh out conditional in quantity using correct default

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -41,7 +41,11 @@ class Bottles {
   }
 
   public function quantity($number=0) {
-    return "no more";
+    if ($number === 0) {
+      return "no more";
+    } else {
+      return $number;
+    }
   }
 
   public function container($number) {
