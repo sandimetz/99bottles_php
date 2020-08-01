a771753343464130a874eee40b5bdecec9e70eaf
Katrina Owen <kytrinyx@github.com>
2020-08-01 17:25:05 -0600

    Send quantity in case 0, phrase 1

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -18,7 +18,8 @@ class Bottles {
     switch ($number) {
     case 0:
       return
-        "No more bottles of beer on the wall, " .
+        ucfirst($this->quantity($number)) . " bottles " .
+          "of beer on the wall, " .
         "no more bottles of beer.\n" .
         "Go to the store and buy some more, " .
         "99 bottles of beer on the wall.\n";
