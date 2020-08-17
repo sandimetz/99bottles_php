02e1b1ea0b5ef2f19b7d195f9a78e1229274bd85
Katrina Owen <kytrinyx@github.com>
2020-08-17 09:35:28 -0600

    Remove temporary guard clause to get back to green

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -80,9 +80,6 @@ class BottleNumber {
   }
 
   public function pronoun() {
-    if (func_num_args() > 0) {
-      throw new Exception('Wrong number of arguments.');
-    }
     if ($this->number === 1) {
       return "it";
     } else {
