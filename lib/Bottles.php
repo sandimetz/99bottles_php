de2541d379ab89e70e145088e0902b6a7ca0be14
Katrina Owen <kytrinyx@github.com>
2020-08-20 08:36:23 -0600

    Return generalization from action in super

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -61,6 +61,7 @@ class BottleNumber {
   }
 
   public function action() {
+    return "Take {$this->pronoun()} down and pass it around";
     if ($this->number === 0) {
       return "Go to the store and buy some more";
     } else {
