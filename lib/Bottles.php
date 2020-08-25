76aacf099842fb92c0ae7d65f5d2aeb095f6c909
Katrina Owen <kytrinyx@github.com>
2020-08-25 10:09:31 -0600

    Parse dispersed choosing logic factory

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -32,6 +32,18 @@ class BottleNumber {
 
   public static function for($number) {
     $classNames = [
+      'BottleNumber6',
+      'BottleNumber1',
+      'BottleNumber0',
+      'BottleNumber',
+    ];
+    foreach ($classNames as $className) {
+      if ($className::handles($number)) {
+        new $className($number);
+      }
+    }
+
+    $classNames = [
       0 => 'BottleNumber0',
       1 => 'BottleNumber1',
       6 => 'BottleNumber6',
