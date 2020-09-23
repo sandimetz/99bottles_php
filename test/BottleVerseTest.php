b965356b0b123164f49e13cdaad079c17822fe11
Katrina Owen <kytrinyx@github.com>
2020-09-23 15:35:22 -0600

    Copy 1st test from BottlesTest to BottleVerseTest

--- test/BottleVerseTest.php
+++ test/BottleVerseTest.php
@@ -3,4 +3,12 @@
 require_once __DIR__ . "/../lib/Bottles.php";
 
 class BottleVerseTest extends \PHPUnit\Framework\TestCase {
+  public function test_the_first_verse() {
+    $expected =
+      "99 bottles of beer on the wall, " .
+      "99 bottles of beer.\n" .
+      "Take one down and pass it around, " .
+      "98 bottles of beer on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verse(99));
+  }
 }
