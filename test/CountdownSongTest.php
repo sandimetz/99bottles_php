091796f2ce1dfeacd244278ed29abe60e5a4557a
Katrina Owen <kytrinyx@github.com>
2020-09-24 09:04:36 -0600

    Delete redundant test for a few verses

--- test/CountdownSongTest.php
+++ test/CountdownSongTest.php
@@ -22,25 +22,6 @@ class CountdownSongTest extends \PHPUnit\Framework\TestCase {
     );
   }
 
-  public function test_a_few_verses() {
-    $expected =
-      "2 bottles of beer on the wall, " .
-      "2 bottles of beer.\n" .
-      "Take one down and pass it around, " .
-      "1 bottle of beer on the wall.\n" .
-      "\n" .
-      "1 bottle of beer on the wall, " .
-      "1 bottle of beer.\n" .
-      "Take it down and pass it around, " .
-      "no more bottles of beer on the wall.\n" .
-      "\n" .
-      "No more bottles of beer on the wall, " .
-      "no more bottles of beer.\n" .
-      "Go to the store and buy some more, " .
-      "99 bottles of beer on the wall.\n";
-    $this->assertEquals($expected, (new CountdownSong())->verses(2, 0));
-  }
-
   public function test_the_whole_song() {
     $expected = <<< SONG
 99 bottles of beer on the wall, 99 bottles of beer.
