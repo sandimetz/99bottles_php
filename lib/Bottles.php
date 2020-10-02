767778be2f348feb60f279d2eb6076043ffb6881
Katrina Owen <kytrinyx@github.com>
2020-10-02 10:18:00 -0600

    Add shameless green example

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -5,86 +5,39 @@ class Bottles {
     return $this->verses(99, 0);
   }
 
-  public function verses(int $bottlesAtStart, int $bottlesAtEnd): string {
-    return implode("\n", array_map([$this, 'verse'], range($bottlesAtStart, $bottlesAtEnd)));
+  public function verses(int $upper, int $lower): string {
+    return implode(
+      "\n",
+      array_map([$this, 'verse'], range($upper, $lower))
+    );
   }
 
-  public function verse(int $bottles): string {
-    return (string)new Round($bottles);
-  }
-}
-
-class Round {
-  public $bottles;
-
-  public function __construct(int $bottles) {
-    $this->bottles = $bottles;
-  }
-
-  public function __toString(): string {
-    return $this->challenge() . $this->response();
-  }
-
-  public function challenge(): string {
-    return ucfirst($this->bottlesOfBeer()) . ' ' .
-      $this->onWall() . ', ' . $this->bottlesOfBeer() . ".\n";
-  }
-
-  public function response(): string {
-    return $this->goToTheStoreOrTakeOneDown() . ', ' .
-      $this->bottlesOfBeer() . ' ' . $this->onWall() . ".\n";
-  }
-
-  public function bottlesOfBeer(): string {
-    return $this->anglicizedBottleCount() . ' ' .
-      $this->pluralizedBottleForm() . ' of ' . $this->beer();
-  }
-
-  public function beer(): string {
-    return 'beer';
-  }
-
-  public function onWall(): string {
-    return 'on the wall';
-  }
-
-  public function pluralizedBottleForm(): string {
-    return $this->lastBeer() ? 'bottle' : 'bottles';
-  }
-
-  public function anglicizedBottleCount(): string {
-    return $this->allOut() ?
-      'no more' : (string)$this->bottles;
-  }
-
-  public function goToTheStoreOrTakeOneDown(): string {
-    if ($this->allOut()) {
-      $this->bottles = 99;
-      return $this->buyNewBeer();
-    } else {
-      $lyrics = $this->drinkBeer();
-      $this->bottles--;
-      return $lyrics;
+  public function verse(int $number): string {
+    switch ($number) {
+    case 0:
+      return
+        "No more bottles of beer on the wall, " .
+        "no more bottles of beer.\n" .
+        "Go to the store and buy some more, " .
+        "99 bottles of beer on the wall.\n";
+    case 1:
+      return
+        "1 bottle of beer on the wall, " .
+        "1 bottle of beer.\n" .
+        "Take it down and pass it around, " .
+        "no more bottles of beer on the wall.\n";
+    case 2:
+      return
+        "2 bottles of beer on the wall, " .
+        "2 bottles of beer.\n" .
+        "Take one down and pass it around, " .
+        "1 bottle of beer on the wall.\n";
+    default:
+      return
+        $number . " bottles of beer on the wall, " .
+        $number . " bottles of beer.\n" .
+        "Take one down and pass it around, " .
+        ($number - 1) . " bottles of beer on the wall.\n";
     }
-  }
-
-  public function buyNewBeer(): string {
-    return 'Go to the store and buy some more';
-  }
-
-  public function drinkBeer(): string {
-    return 'Take ' . $this->itOrOne() . ' down and pass it around';
-  }
-
-  public function itOrOne(): string {
-    return $this->lastBeer() ? 'it' : 'one';
-  }
-
-  public function allOut(): bool {
-    return $this->bottles === 0;
-  }
-
-  public function lastBeer(): bool {
-    return $this->bottles === 1;
   }
 }
