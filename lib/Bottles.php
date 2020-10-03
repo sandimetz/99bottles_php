57158699c5cf52235a52d15c3a78d621b3faa02e
Katrina Owen <kytrinyx@github.com>
2020-10-03 14:18:58 -0600

    Pass 3 and 99 with conditional

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -1,11 +1,19 @@
 <?php
 
 class Bottles {
-  public function verse(): string {
-    return
-      "99 bottles of beer on the wall, " .
-      "99 bottles of beer.\n" .
-      "Take one down and pass it around, " .
-      "98 bottles of beer on the wall.\n";
+  public function verse(int $number): string {
+    if ($number === 99) {
+      return
+        "99 bottles of beer on the wall, " .
+        "99 bottles of beer.\n" .
+        "Take one down and pass it around, " .
+        "98 bottles of beer on the wall.\n";
+    } else {
+      return
+        "3 bottles of beer on the wall, " .
+        "3 bottles of beer.\n" .
+        "Take one down and pass it around, " .
+        "2 bottles of beer on the wall.\n";
+    }
   }
 }
