fd177727c7e4124c81dae5c3c2f48e533ae3ba7b
Katrina Owen <kytrinyx@github.com>
2020-10-03 14:48:44 -0600

    Pass test for verse 1

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -2,13 +2,20 @@
 
 class Bottles {
   public function verse(int $number): string {
-    if ($number === 2) {
+    switch ($number) {
+    case 1:
+      return
+        "1 bottle of beer on the wall, " .
+        "1 bottle of beer.\n" .
+        "Take it down and pass it around, " .
+        "no more bottles of beer on the wall.\n";
+    case 2:
       return
         "2 bottles of beer on the wall, " .
         "2 bottles of beer.\n" .
         "Take one down and pass it around, " .
         "1 bottle of beer on the wall.\n";
-    } else {
+    default:
       return
         $number . " bottles of beer on the wall, " .
         $number . " bottles of beer.\n" .
