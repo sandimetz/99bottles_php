4a9616ae39f3cf542485fa39c3ab53c8a2b2bca3
Katrina Owen <kytrinyx@github.com>
2020-10-03 14:54:59 -0600

    Make verses test pass with literal

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -1,6 +1,19 @@
 <?php
 
 class Bottles {
+  public function verses(): string {
+    return
+      "99 bottles of beer on the wall, " .
+      "99 bottles of beer.\n" .
+      "Take one down and pass it around, " .
+      "98 bottles of beer on the wall.\n" .
+      "\n" .
+      "98 bottles of beer on the wall, " .
+      "98 bottles of beer.\n" .
+      "Take one down and pass it around, " .
+      "97 bottles of beer on the wall.\n";
+  }
+
   public function verse(int $number): string {
     switch ($number) {
     case 0:
