07d99a30b41382f156a1803180a0f58457f4633d
Katrina Owen <kytrinyx@github.com>
2020-10-03 14:53:17 -0600

    Add test for verses 99 and 98

--- test/BottlesTest.php
+++ test/BottlesTest.php
@@ -47,4 +47,18 @@ class BottlesTest extends \PHPUnit\Framework\TestCase {
       "99 bottles of beer on the wall.\n";
     $this->assertEquals($expected, (new Bottles())->verse(0));
   }
+
+  public function test_a_couple_verses() {
+    $expected =
+      "99 bottles of beer on the wall, " .
+      "99 bottles of beer.\n" .
+      "Take one down and pass it around, " .
+      "98 bottles of beer on the wall.\n" .
+      "\n" .
+      "98 bottles of beer on the wall, " .
+      "98 bottles of beer.\n" .
+      "Take one down and pass it around, " .
+      "97 bottles of beer on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verses(99, 98));
+  }
 }
