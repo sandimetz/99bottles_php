255cde8583344cb39ddbc6b20c0c0746d90036d6
Katrina Owen <kytrinyx@github.com>
2020-10-03 15:01:32 -0600

    Add test for verses 2, 1, and 0

--- test/BottlesTest.php
+++ test/BottlesTest.php
@@ -61,4 +61,23 @@ class BottlesTest extends \PHPUnit\Framework\TestCase {
       "97 bottles of beer on the wall.\n";
     $this->assertEquals($expected, (new Bottles())->verses(99, 98));
   }
+
+  public function test_a_few_verses() {
+    $expected =
+      "2 bottles of beer on the wall, " .
+      "2 bottles of beer.\n" .
+      "Take one down and pass it around, " .
+      "1 bottle of beer on the wall.\n" .
+      "\n" .
+      "1 bottle of beer on the wall, " .
+      "1 bottle of beer.\n" .
+      "Take it down and pass it around, " .
+      "no more bottles of beer on the wall.\n" .
+      "\n" .
+      "No more bottles of beer on the wall, " .
+      "no more bottles of beer.\n" .
+      "Go to the store and buy some more, " .
+      "99 bottles of beer on the wall.\n";
+    $this->assertEquals($expected, (new Bottles())->verses(2, 0));
+  }
 }
