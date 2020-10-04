59393df7d03fac341867b6253a79bdb74295434d
Katrina Owen <kytrinyx@github.com>
2020-10-04 17:13:06 -0600

    Use successor in place of number-1 everywhere

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -22,16 +22,16 @@ class Bottles {
           " of beer on the wall, " .
         $this->quantity($number) . " " . $this->container($number) . " of beer.\n" .
         $this->action($number) . ", " .
-        $this->quantity($this->successor($number)) . " " . $this->container($number-1) .
-          " of beer on the wall.\n";
+        $this->quantity($this->successor($number)) . " " .
+          $this->container($this->successor($number)) . " of beer on the wall.\n";
     default:
       return
         ucfirst($this->quantity($number)) . " " . $this->container($number) .
           " of beer on the wall, " .
         $this->quantity($number) . " " . $this->container($number) . " of beer.\n" .
         $this->action($number) . ", " .
-        $this->quantity($this->successor($number)) . " " . $this->container($number-1) .
-          " of beer on the wall.\n";
+        $this->quantity($this->successor($number)) . " " .
+          $this->container($this->successor($number)) . " of beer on the wall.\n";
     }
   }
 
