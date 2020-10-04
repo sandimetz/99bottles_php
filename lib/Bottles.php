6e6f21ae358f553c3b29af19d24bd2b9985e0e7d
Katrina Owen <kytrinyx@github.com>
2020-10-04 15:42:54 -0600

    Revert to green

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -39,11 +39,7 @@ class Bottles {
   }
 
   public function quantity($number="FIXME") {
-    if ($number === 0) {
-      return "no more";
-    } else {
-      return $number;
-    }
+    return "no more";
   }
 
   public function container(int $number): string {
