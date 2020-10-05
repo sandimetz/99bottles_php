0020ac99e243b33c638c6613ceff5a3a4abef81a
Katrina Owen <kytrinyx@github.com>
2020-10-05 14:57:28 -0600

    Remove temporary guard clause from successor

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -81,9 +81,6 @@ class BottleNumber {
   }
 
   public function successor(): int {
-    if (func_num_args() > 0) {
-      throw new Exception('Wrong number of arguments.');
-    }
     if ($this->number === 0) {
       return 99;
     } else {
