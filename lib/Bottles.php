2d776f793d9f892912b1f260f4b34534fc4930e4
Katrina Owen <kytrinyx@github.com>
2020-10-05 14:41:29 -0600

    Add temporary guard clause to quantity

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -53,6 +53,9 @@ class BottleNumber {
   }
 
   public function quantity(): string {
+    if (func_num_args() > 0) {
+      throw new Exception('Wrong number of arguments.');
+    }
     if ($this->number === 0) {
       return "no more";
     } else {
