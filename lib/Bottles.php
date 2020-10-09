ba703983ecff0c249eab97467bb945f13611453a
Katrina Owen <kytrinyx@github.com>
2020-10-09 15:10:06 -0600

    Implement quantity for BottleNumber6

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -100,6 +100,10 @@ class BottleNumber1 extends BottleNumber {
 }
 
 class BottleNumber6 extends BottleNumber {
+  public function quantity(): string {
+    return "1";
+  }
+
   public function container(): string {
     return "six-pack";
   }
