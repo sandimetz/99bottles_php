9f9c1720730181b4cd7888f1952e38b37dc2f520
Katrina Owen <kytrinyx@github.com>
2020-10-09 12:58:37 -0600

    Delete obsolete conditional from container in subclass

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -92,10 +92,5 @@ class BottleNumber0 extends BottleNumber {
 class BottleNumber1 extends BottleNumber {
   public function container(): string {
     return "bottle";
-    if ($this->number === 1) {
-      return "bottle";
-    } else {
-      return "bottles";
-    }
   }
 }
