b47c0eb7d123831995e66850d1a035cf8f85753b
Katrina Owen <kytrinyx@github.com>
2020-10-10 12:57:22 -0600

    Use results of lyrics

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -15,7 +15,7 @@ class Bottles {
   }
 
   public function verse(int $number): string {
-    // return (new BottleVerse($number))->lyrics();
+    return (new BottleVerse($number))->lyrics();
     $bottleNumber = BottleNumber::for($number);
 
     return
