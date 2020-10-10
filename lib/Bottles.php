53a36070ad890f6341162f461fc43a298e111420
Katrina Owen <kytrinyx@github.com>
2020-10-10 11:57:16 -0600

    Parse self-registering factory

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -34,6 +34,12 @@ class BottleNumber {
   }
 
   public static function for(int $number): BottleNumber {
+    foreach (self::$registry as $candidate) {
+      if ($candidate::handles($number)) {
+        new $candidate($number);
+      }
+    }
+
     $classNames = [
       'BottleNumber6',
       'BottleNumber1',
