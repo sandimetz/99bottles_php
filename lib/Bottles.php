1b1d7c94feed1bd7422c7eed4912d7b01d79f171
Katrina Owen <kytrinyx@github.com>
2020-10-10 15:54:28 -0600

    Duplicate Bottles as CountdownSong

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -2,6 +2,29 @@
 
 declare(strict_types = 1);
 
+class CountdownSong {
+  protected $verseTemplate;
+
+  public function __construct(string $verseTemplate = BottleVerse::class) {
+    $this->verseTemplate = $verseTemplate;
+  }
+
+  public function song(): string {
+    return $this->verses(99, 0);
+  }
+
+  public function verses(int $upper, int $lower): string {
+    return implode(
+      "\n",
+      array_map([$this, 'verse'], range($upper, $lower))
+    );
+  }
+
+  public function verse(int $number): string {
+    return $this->verseTemplate::lyrics($number);
+  }
+}
+
 class Bottles {
   protected $verseTemplate;
 
