88fbd11e0bef8f12689c39e0c8454dfa314f211a
Katrina Owen <kytrinyx@github.com>
2020-10-10 12:45:33 -0600

    Copy verse from Bottles to BottleVerse

--- lib/Bottles.php
+++ lib/Bottles.php
@@ -31,6 +31,16 @@ class BottleVerse {
   public function __construct(int $number) {
     $this->number = $number;
   }
+
+  public function verse(int $number): string {
+    $bottleNumber = BottleNumber::for($number);
+
+    return
+      ucfirst("{$bottleNumber} of beer on the wall, ") .
+      "{$bottleNumber} of beer.\n" .
+      "{$bottleNumber->action()}, " .
+      "{$bottleNumber->successor()} of beer on the wall.\n";
+  }
 }
 
 class BottleNumber {
